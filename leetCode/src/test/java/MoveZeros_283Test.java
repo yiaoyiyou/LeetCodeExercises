939e6cc3eef4138a48c3f8101fb494ec25d7a429
1278882395@qq.com <1278882395@qq.com>
2022-04-23 15:58:31 +0800

    增加测试用例“

--- leetCode/src/test/java/MoveZeros_283Test.java
+++ leetCode/src/test/java/MoveZeros_283Test.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoveZeros_283Test {
@@ -16,6 +18,9 @@ class MoveZeros_283Test {
 
     @Test
     void moveZeroes() {
-
+        MoveZeros_283 moveZeros_283 = new MoveZeros_283();
+        int[] testData = new int[]{1, 0, 2, 0, 3};
+        moveZeros_283.moveZeroes(testData);
+        System.out.println(Arrays.toString(testData));
     }
 }
