37d2ba527632e262de45c233a1201f7f84332e68
1278882395@qq.com <1278882395@qq.com>
2022-04-23 17:20:12 +0800

    test

---
+++ leetCode/src/test/java/solutions/RemoveElement_23Test.java
@@ -0,0 +1,12 @@
+package solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveElement_23Test {
+
+    @Test
+    void removeElement() {
+    }
+}
