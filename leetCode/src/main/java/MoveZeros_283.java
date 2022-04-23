2e144537e6a12a6b41ab183d7a85a3b1bc0b83a9
1278882395@qq.com <1278882395@qq.com>
2022-04-23 15:49:36 +0800

    增加283号ç­

--- leetCode/src/main/java/MoveZeros_283.java
+++ leetCode/src/main/java/MoveZeros_283.java
@@ -1,3 +1,9 @@
+/**
+ * @Author weike
+ * @Date 2022/4/23 15:48
+ * @ClassName MoveZeros_283
+ * @Link https://leetcode.com/problems/move-zeroes/
+ */
 public class MoveZeros_283 {
     public void moveZeroes(int[] nums) {
         int zeroNum = 0;
