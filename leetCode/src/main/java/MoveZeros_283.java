3a0512fc15048cbd45bd7cfa87577df79d08cf7a
1278882395@qq.com <1278882395@qq.com>
2022-04-23 16:00:55 +0800

    优å简约代码“

--- leetCode/src/main/java/MoveZeros_283.java
+++ leetCode/src/main/java/MoveZeros_283.java
@@ -6,18 +6,9 @@
  */
 public class MoveZeros_283 {
     public void moveZeroes(int[] nums) {
-        int zeroNum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                zeroNum++;
-            }
-        }
-        if (zeroNum == 0) {
-            return;
-        }
         // 记录非零范围[0,j]
         int j = -1;
-        for (int i = 0; i < nums.length && j < nums.length; i ++) {
+        for (int i = 0; i < nums.length; i ++) {
             if (nums[i] != 0) {
                 j ++;
                 nums[j] = nums[i];
