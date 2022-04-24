d5479b38fe728992be31be35b1e609163f0f92fc
1278882395@qq.com <1278882395@qq.com>
2022-04-24 23:58:04 +0800

    增加125号题目，使用对撞指针å法

---
+++ leetCode/src/main/java/solutions/ValidPalindrome_125.java
@@ -0,0 +1,42 @@
+package solutions;
+
+/**
+ * @Author weike
+ * @Date 2022/4/24 23:21
+ * @ClassName ValidPalindrome_125
+ * @Link https://leetcode.com/problems/valid-palindrome/
+ * @Analysis
+ */
+public class ValidPalindrome_125 {
+    public boolean isPalindrome(String s) {
+        if (s == null){
+            throw new IllegalArgumentException("null input");
+        }
+        if (s.isBlank()) {
+            return true;
+        }
+        int i = 0, j = s.length() - 1;
+        s = s.toLowerCase();
+        while (i < j) {
+            while (i < s.length() && !isAlph(s.charAt(i))) {
+                i++;
+            }
+            while (j >= 0 && !isAlph(s.charAt(j))) {
+                j--;
+            }
+            if (i > j) {
+                return true;
+            }
+            if (s.charAt(i) == s.charAt(j)) {
+                i ++;
+                j --;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+    private boolean isAlph(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+    }
+}
---
+++ leetCode/src/test/java/solutions/ValidPalindrome_125Test.java
@@ -0,0 +1,14 @@
+package solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidPalindrome_125Test {
+
+    @Test
+    void isPalindrome() {
+        ValidPalindrome_125 validPalindrome_125 = new ValidPalindrome_125();
+        validPalindrome_125.isPalindrome(".,");
+    }
+}
