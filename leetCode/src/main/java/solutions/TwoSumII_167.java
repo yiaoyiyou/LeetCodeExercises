18a027eabca4db0095d6e4ec3271b31c04f38363
1278882395@qq.com <1278882395@qq.com>
2022-04-24 23:03:17 +0800

    增加对撞指针解法â

--- leetCode/src/main/java/solutions/TwoSumII_167.java
+++ leetCode/src/main/java/solutions/TwoSumII_167.java
@@ -5,7 +5,7 @@ package solutions;
  * @Date 2022/4/24 9:00
  * @ClassName TwoSumII_167
  * @Link https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
- * @Analysis 二分查找法,双索引法
+ * @Analysis 二分查找法, 双索引法
  */
 public class TwoSumII_167 {
     public int[] twoSum(int[] numbers, int target) {
@@ -14,7 +14,7 @@ public class TwoSumII_167 {
             //边界条件需要维护
             int j = binarySearch(numbers, target - numbers[i], i + 1, numbers.length - 1);
             if (j != -1) {
-                res[0] = i +1;
+                res[0] = i + 1;
                 res[1] = j + 1;
                 return res;
             }
@@ -36,4 +36,24 @@ public class TwoSumII_167 {
             return binarySearch(numbers, target, mid + 1, end);
         }
     }
+
+    public int[] twoSumV1(int[] numbers, int target) {
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target){
+                int[] res = new int[2];
+                res[0] = i + 1;
+                res[1] = j + 1;
+                return res;
+            } else if (sum > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return new int[]{0, 0};
+
+    }
+
 }
