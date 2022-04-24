b73f0b51c6077832808020329552dd521ce79516
1278882395@qq.com <1278882395@qq.com>
2022-04-24 09:20:58 +0800

    二分查找法

---
+++ leetCode/src/main/java/solutions/TwoSumII_167.java
@@ -0,0 +1,39 @@
+package solutions;
+
+/**
+ * @Author weike
+ * @Date 2022/4/24 9:00
+ * @ClassName TwoSumII_167
+ * @Link https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+ * @Analysis 二分查找法
+ */
+public class TwoSumII_167 {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < numbers.length; i++) {
+            //边界条件需要维护
+            int j = binarySearch(numbers, target - numbers[i], i + 1, numbers.length - 1);
+            if (j != -1) {
+                res[0] = i +1;
+                res[1] = j + 1;
+                return res;
+            }
+        }
+        return res;
+    }
+
+    private int binarySearch(int[] numbers, int target, int start, int end) {
+        //因为是[start, end]
+        if (start > end) {
+            return -1;
+        }
+        int mid = start + (end - start) / 2;
+        if (numbers[mid] == target) {
+            return mid;
+        } else if (numbers[mid] > target) {
+            return binarySearch(numbers, target, start, mid - 1);
+        } else {
+            return binarySearch(numbers, target, mid + 1, end);
+        }
+    }
+}
